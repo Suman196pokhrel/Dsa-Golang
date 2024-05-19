d3493b524229bec2d575cc3123f262e7de8d9d83
Suman Pokhrel <spokhrel196@gmail.com>
2024-05-19 10:08:05 +0545

    added length function for DLL

--- datastructures/linkedListGo/doubleLinkedLIst.go
+++ datastructures/linkedListGo/doubleLinkedLIst.go
@@ -30,6 +30,21 @@ func (dll *dLinkedList) displayElements() error {
 	return nil
 }
 
+func (dll *dLinkedList) length() int {
+	if dll.head == nil {
+		return 0
+	}
+
+	curr := dll.head
+	res := 0
+	for curr != nil {
+		res++
+		curr = curr.next
+	}
+
+	return res
+}
+
 func (dll *dLinkedList) insertAtBeginning(userData int) {
 	node := &dnode{
 		data: userData,
@@ -64,7 +79,34 @@ func (dll *dLinkedList) insertAtEnd(userData int) {
 
 		return
 	}
+}
 
+func (dll *dLinkedList) insertInIndex(insertIndex int, userData int) {
+
+	if dll.head == nil {
+		fmt.Printf("Double LinkedList is empty ! inserting at first")
+		dll.insertAtBeginning(userData)
+		return
+	}
+
+	node := dnode{
+		data: userData,
+	}
+	current := dll.head
+
+	for i := 0; i < insertIndex-1; i++ {
+		current = current.next
+	}
+
+	// NODE BEING INSERTED
+	node.next = current.next
+	node.prev = current
+
+	// AFTER NODE
+	current.next.prev = &node
+
+	// BEFORE NODE
+	current.next = &node
 }
 
 func DoubleLinkedListInGo() {
@@ -78,8 +120,16 @@ func DoubleLinkedListInGo() {
 	dll.insertAtBeginning(15)
 	dll.displayElements()
 
+	fmt.Println("LENGTH => ", dll.length())
+
 	dll.insertAtEnd(10)
 	dll.displayElements()
+	fmt.Println("LENGTH => ", dll.length())
+
 	// dll.insertAtEnd(10)
+
+	dll.insertInIndex(2, 222)
+	dll.displayElements()
+	fmt.Println("LENGTH => ", dll.length())
 
 }
